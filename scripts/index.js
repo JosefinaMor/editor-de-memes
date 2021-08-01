764266133bb9584245d4521d9e27a5227190bbb3
JosefinaMor <josefina.morales.1765@gmail.com>
2021-08-01 12:27:26 -0300

    agrego botones para agregar los aside de texto e imagen

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
-const cambioBody      = document.querySelector("body");
-const botonModoClaro  = document.getElementById("modo-claro");
-const barraNavegacion = document.getElementById("barraNavegacion");
-const asideImagen     = document.getElementById("asideImagen");
-const asideTexto      = document.getElementById("asideTexto");
+const cambioBody       = document.querySelector("body");
+const botonModoClaro   = document.getElementById("modo-claro");
+const barraNavegacion  = document.getElementById("barraNavegacion");
+const asideImagen      = document.getElementById("asideImagen");
+const asideTexto       = document.getElementById("asideTexto");
+const botonAsideTexto  = document.getElementById("botonAsideTexto");
+const botonAsideImagen = document.getElementById("botonAsideImagen");
+const botonEscape      = document.getElementById("botonEscape");
 
 const cambioDeModo = () =>{
     cambioBody.classList.toggle("modoClaro");
@@ -11,4 +14,16 @@ const cambioDeModo = () =>{
     asideTexto.classList.toggle("modoClaroAside");
 }
 
-botonModoClaro.onclick = cambioDeModo;
+const aparecerAsideTexto = () =>{
+    asideTexto.classList.toggle("mostrarAside"); 
+    asideImagen.classList.remove("mostrarAside");
+}
+
+const aparecerAsideImagen = () =>{
+    asideImagen.classList.toggle("mostrarAside"); 
+    asideTexto.classList.remove("mostrarAside");
+}
+
+botonModoClaro.onclick   = cambioDeModo;
+botonAsideTexto.onclick  = aparecerAsideTexto;
+botonAsideImagen.onclick = aparecerAsideImagen;
