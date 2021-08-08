0a8158379f854559361abc24c90720e6017fbe29
JosefinaMor <josefina.morales.1765@gmail.com>
2021-08-08 10:35:12 -0300

    agrego funcionalidad de insertar imagen

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,56 @@
-const cambioBody       = document.querySelector("body");
-const botonModoClaro   = document.getElementById("modo-claro");
-const barraNavegacion  = document.getElementById("barraNavegacion");
-const asideImagen      = document.getElementById("asideImagen");
-const asideTexto       = document.getElementById("asideTexto");
-const botonAsideTexto  = document.getElementById("botonAsideTexto");
-const botonAsideImagen = document.getElementById("botonAsideImagen");
-const botonEscape      = document.getElementById("botonEscape");
+const cambioBody        = document.querySelector("body");
+const botonModoClaro    = document.getElementById("modo-claro");
+const barraNavegacion   = document.getElementById("barraNavegacion");
+const botonesDelNav     = document.getElementById("botonesDelNav");
+const asideImagen       = document.getElementById("asideImagen");
+const asideTexto        = document.getElementById("asideTexto");
+const botonAsideTexto   = document.getElementById("botonAsideTexto");
+const botonAsideImagen  = document.getElementById("botonAsideImagen");
+const botonEscapeTexto  = document.getElementById("botonEscapeTexto");
+const botonEscapeImagen = document.getElementById("botonEscapeImagen");
+const fotoMeme          = document.getElementById("foto-meme");
+const urlImagen         = document.getElementById("url-imagen");
 
 const cambioDeModo = () =>{
     cambioBody.classList.toggle("modoClaro");
     barraNavegacion.classList.toggle("modoClaroBarraNvegacion");
     asideImagen.classList.toggle("modoClaroAside");
     asideTexto.classList.toggle("modoClaroAside");
+    botonesDelNav.classList.toggle("modoClaroLi");
+}
+
+const insertaImagen = () =>{
+    const valor = urlImagen.value;
+    fotoMeme.src = valor;
 }
 
 const aparecerAsideTexto = () =>{
     asideTexto.classList.toggle("mostrarAside"); 
     asideImagen.classList.remove("mostrarAside");
+    botonAsideTexto.classList.toggle("remarcoBoton");
+    botonAsideImagen.classList.remove("remarcoBoton");
 }
 
 const aparecerAsideImagen = () =>{
     asideImagen.classList.toggle("mostrarAside"); 
     asideTexto.classList.remove("mostrarAside");
+    botonAsideImagen.classList.toggle("remarcoBoton");
+    botonAsideTexto.classList.remove("remarcoBoton");
 }
 
-botonModoClaro.onclick   = cambioDeModo;
-botonAsideTexto.onclick  = aparecerAsideTexto;
-botonAsideImagen.onclick = aparecerAsideImagen;
+const salirAsideTexto = () =>{
+    asideTexto.classList.remove("mostrarAside");
+    botonAsideTexto.classList.remove("remarcoBoton");
+}
+
+const salirAsideImagen = () =>{
+    asideImagen.classList.remove("mostrarAside");
+    botonAsideImagen.classList.remove("remarcoBoton");
+}
+
+botonModoClaro.onclick    = cambioDeModo;
+botonAsideTexto.onclick   = aparecerAsideTexto;
+botonAsideImagen.onclick  = aparecerAsideImagen;
+botonEscapeTexto.onclick  = salirAsideTexto;
+botonEscapeImagen.onclick = salirAsideImagen;
+urlImagen.onblur          = insertaImagen;
